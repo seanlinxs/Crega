b2a30c04c0c0e62406c2acf3cc6dde080c8826b3
Sean Lin <sean.linxs@gmail.com>
2015-12-13 21:43:56 +1100

    Add view logic for home page

--- main/views.py
+++ main/views.py
@@ -1,8 +1,20 @@
 from django.views.generic import TemplateView
+from main.models import *
 
 
 class Home(TemplateView):
     template_name = 'main/home.html'
+    
+
+    def get_context_data(self, **kwargs):
+        context = super(Home, self).get_context_data(**kwargs)
+        page = Page.objects.get(pk=1)
+        textblocks = page.textblock_set;
+        primary_box_text = textblocks.get(name='Text block #1')
+        context['page_name'] = page.name
+        context['primary_box_text'] = primary_box_text.content
+
+        return context
 
 
 class About(TemplateView):
